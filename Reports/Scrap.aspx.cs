7886b3e4ea7694c9b04ffc53dc45f1c678f859e4
Javi505amx <javiervp41@gmail.com>
2024-05-31 14:48:01 -0600

    feat(code) : Add code behind

---
+++ Reports/Scrap.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FinishGoodSMT.Reports
+{
+    public partial class Scrap : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CancelBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void QueryBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void filterText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SearchBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RefreshBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
